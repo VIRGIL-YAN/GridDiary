f92d1230ae10257e6fbbfa354ff21b2aa798bd69
七哥 <studiotang.cn@gmail.com>
2016-05-20 00:22:29 +0800

    增加沉浸式状态栏

--- app/src/main/java/com/free/diary/ui/activity/BaseActivity.java
+++ app/src/main/java/com/free/diary/ui/activity/BaseActivity.java
@@ -1,14 +1,26 @@
 package com.free.diary.ui.activity;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.free.diary.R;
+import com.free.diary.ui.view.SystemBarTintManager;
 
 /**
  * Created by tangqi on 16/5/18.
  */
 public abstract class BaseActivity extends Activity implements OnClickListener {
+
+    /**
+     * 系统状态栏管理
+     */
+    protected SystemBarTintManager mSystemBarTintManager;
 
     /**
      * 设置布局文件
@@ -39,6 +51,11 @@ public abstract class BaseActivity extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 初始化系统状态栏
+        if (isUseSystemBarTint()) {
+            initSystemBarTint();
+        }
+
         setContentLayout();
         beforeInitView();
         initView();
@@ -63,5 +80,82 @@ public abstract class BaseActivity extends Activity implements OnClickListener {
         }
 
         onViewClick(view);
+    }
+
+    /**
+     * 初始化系统状态栏
+     */
+    protected void initSystemBarTint() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 状态栏透明 需要在创建SystemBarTintManager 之前调用。
+            setTranslucentStatus(true);
+        }
+
+        // create our manager instance after the content view is set
+        mSystemBarTintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        mSystemBarTintManager.setStatusBarTintEnabled(true);
+        // enable navigation bar tint
+        mSystemBarTintManager.setNavigationBarTintEnabled(true);
+        // set a custom tint color for all system bars
+        mSystemBarTintManager.setTintColor(getTintColor());
+
+        // set a custom navigation bar resource
+        // mSystemBarTintManager.setNavigationBarTintResource(R.drawable.ic_launcher);
+        // set a custom status bar drawable
+//        mSystemBarTintManager.setStatusBarTintResource(R.drawable.colorPrimary);
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    /**
+     * 是否使用沉浸式状态栏
+     *
+     * @return
+     */
+    protected boolean isUseSystemBarTint() {
+        return true;
+    }
+
+    /**
+     * 获取系统状态栏颜色
+     *
+     * @return
+     */
+    protected int getTintColor() {
+        return getResources().getColor(R.color.colorPrimary);
+    }
+
+    /**
+     * 设置系统状态栏颜色
+     *
+     * @param color
+     */
+    protected void setTintColor(int color) {
+        if (mSystemBarTintManager != null) {
+            mSystemBarTintManager.setTintColor(color);
+        }
+    }
+
+    /**
+     * 设置系统状态栏背景
+     *
+     * @param resId
+     */
+    protected void setTintResource(int resId) {
+        if (mSystemBarTintManager != null) {
+            mSystemBarTintManager.setStatusBarTintResource(resId);
+        }
     }
 }
